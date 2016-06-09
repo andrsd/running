99b1566bac41d621ff77e39e74b6c6c216ff6c5c
David Andrs <andrsd@gmail.com>
2016-06-08 18:00:32 -0600

    Replacing mockup values of weight measurements

--- _plugins/time.rb
+++ _plugins/time.rb
@@ -13,6 +13,16 @@ module Jekyll
             secs = time % 60
             mins.to_i.to_s + ':' + secs.to_i.to_s
         end
+
+        def wdate(str)
+            year = str[0, 4].to_i
+            month = str[4, 2].to_i
+            day = str[6, 2].to_i
+            hour = str[9, 2].to_i
+            minute = str[11, 2].to_i
+            d = Time.new(year, month, day, hour, minute, 0)
+            d.strftime('%b %d, %Y') + ' at ' + d.strftime('%H:%M')
+        end
     end
 end
 
